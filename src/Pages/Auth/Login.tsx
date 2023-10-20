a0ab7968707c0a7b02b7ab0c60fb3c60c29bc24b
Jeremiah Longmatey <longmateyjeremiah1@gmail.com>
2023-10-20 11:38:58 +0000

    added login page

--- src/Pages/Auth/Login.tsx
+++ src/Pages/Auth/Login.tsx
@@ -1,10 +1,44 @@
 import React from "react";
 import { AuthLayout } from "../../layout";
+import { Box, Center, HStack, Text, VStack } from "@chakra-ui/react";
+import { Button, Heading, Input } from "../../components/ui";
+import { Link } from "react-router-dom";
 
 export function Login() {
   return (
     <AuthLayout>
-      <p>This is the Login Screen</p>
+      <VStack
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap={4}
+        h={"full"}
+        w={["full", 350]}
+        px={[6, 0]}
+      >
+        <Box textAlign={"center"}>
+          <Heading fontSize={["3xl", "4xl"]}>Login Page</Heading>
+          <Text fontSize={["14px"]} pt={2} color={"gray.500"}>
+            Hey! we missed you, Welcome back
+          </Text>
+        </Box>
+        <Box w={"full"} gap={6} display={"flex"} flexDirection={"column"}>
+          <Input placeholder="name" type="text" />
+          <Input placeholder="password" type="password" />
+          <Box>
+            <Button w={"full"}>Login</Button>
+          </Box>
+          <Box>
+            <Center>
+              <HStack fontSize="14px" fontWeight={600}>
+                <Text color={"gray.400"}>Don't have an account?</Text>
+                <Text color="primary.200">
+                  <Link to="/register">Sign Up</Link>
+                </Text>
+              </HStack>
+            </Center>
+          </Box>
+        </Box>
+      </VStack>
     </AuthLayout>
   );
 }
